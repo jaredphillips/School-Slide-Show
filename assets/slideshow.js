2fca99977d1d532aabab5d8fa584a900247af5b0
Jared Phillips <jaredtphillips@me.com>
2013-08-01 13:03:47 -0400

    Url fixed and some tips from Khurram

--- assets/slideshow.js
+++ assets/slideshow.js
@@ -1,25 +1,29 @@
+// Angular app sits here. 
 var slideshow = angular.module('slideshow', []).config(
-		['$routeProvider', '$locationProvider', function( $routeProvider, $locationProvider ) {
-			$routeProvider.when('/tag/:tag');
+		['$routeProvider', function( $routeProvider ) {
+			$routeProvider.when('/');
 		}]
-  	).controller(
-		'slideshow', function ( $scope, $http, $timeout, $route, $location ) {
-			// Set the API endpoint
-			var api = 'https://api.instagram.com/v1/tags/bitmakerlabs/media/recent?access_token=257058201.9af4692.3d68e63b114944a0be332da732923a23&callback=JSON_CALLBACK',
+ 	)
+
+// Create slideshow controller
+slideshow.controller(
+		'slideshow', function ( $scope, $http, $timeout, $route ) {
+
+			// set the api url
+			var url = 'https://api.instagram.com/v1/tags/bitmakerlabs/media/recent?access_token=257058201.9af4692.3d68e63b114944a0be332da732923a23&callback=JSON_CALLBACK',
 				newReq, refreshApi;
 
 			$scope.fetchImages = function() {
 
-				$scope.loadingClass = 'loading';
 				$scope.imgCurrent = 0;
 
 				if ( ! $route.current )
-					$location.path( '/tag/' + $scope.tag );
+					$location.path( url );
 				else if ( angular.isDefined( $route.current.params.tag ) )
 					$scope.tag = $route.current.params.tag;
-
-				$http.jsonp( 
-					api.replace( '%tag%', $scope.tag )
+     
+      // Use jsonp to interact with api 
+				$http.jsonp(url
 				).success( function( data ) {
 					delete $scope.loadingClass;
 
@@ -42,22 +46,13 @@ var slideshow = angular.module('slideshow', []).config(
 				});
 			}
 
-			// Fetch images
 			$timeout( $scope.fetchImages );
 
 			$scope.advanceSlide = function() {
-				// Method 1
 				// Use a classname to highlight the current active slide
 				if ( angular.isDefined( $scope.images ) && $scope.images.length )
 					$scope.makeActiveSlide( $scope.imgCurrent + 1 );
-
-				/*
-				// Method 2
-				// Just flush the array elements around
-				if ( angular.isDefined( $scope.images ) )
-					$scope.images.push( $scope.images.shift() );
-				*/
-
+      // Set time for advancing slide
 				$timeout( $scope.advanceSlide, 4000 );
 			}
 
@@ -72,23 +67,5 @@ var slideshow = angular.module('slideshow', []).config(
 				// Activate the next slide
 				$scope.images[ $scope.imgCurrent ].isActive = true;
 			}
-
-			$scope.tagChange = function() {
-				$location.path( '/tag/' + $scope.tag );
-
-				if ( newReq )
-					$timeout.cancel( newReq );
-
-				newReq = $timeout( function() {
-					$scope.fetchImages();
-					$timeout.cancel( newReq );
-				}, 1000);
-			}
 		}
-	).filter(
-		'escape', function () {
-			return function( input ) {
-				return escape( input );
-			}
-		}	
 	);
